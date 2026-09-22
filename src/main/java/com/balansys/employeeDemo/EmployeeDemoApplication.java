a9480c8f44addcee251abf2ca3352dfa270ed603
Bmaniz63 <bmaniz63@gmail.com>
2026-09-22 13:01:22 -0400

    Updated the name of the application everywhere.

--- src/main/java/com/balansys/employeeDemo/EmployeeDemoApplication.java
+++ src/main/java/com/balansys/employeeDemo/EmployeeDemoApplication.java
@@ -1,4 +1,4 @@
-package com.balansys.employee_demo;
+package com.balansys.employeeDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/balansys/employeeDemo/EmployeeDemoApplicationTests.java
+++ src/test/java/com/balansys/employeeDemo/EmployeeDemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.balansys.employee_demo;
+package com.balansys.employeeDemo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
